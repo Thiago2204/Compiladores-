3fd43d05d42246134e860c36480f5bdb35c5a6ca
RysanDeluna <ry.marco.andrade@gmail.com>
2023-04-07 23:14:42 -0300

    Criação dos DEFINES e separação dos arquivos

--- Analisador léxico/analisador_lexico.c
+++ Analisador léxico/analisador_lexico.c
@@ -1,48 +1,64 @@
-// ANALISADOR LÉXICO - ETAPA 2 -----------------------------------------------
+// ANALISADOR LÉXICO - ETAPA 2 ================================================
 
-// Inclusões------------------------------------------------------------------
-#include <stdio.h>
+// Inclusões ==================================================================
 
-// Definições ---------------------------------------------------------------
-#define _ACEITA_ 1
-#define _REJEITA_ 0
+// Definições =================================================================
+// Palavras reservadas --------------------------------------------------------
+#define _FALSE_ 0
+#define _TRUE_  1
+#define _BOOL_  3
+#define _INT_   4
+#define _VOID_  5
+#define _SEMIC_ 6
+#define _PRINT_ 7
+#define _PROC_  8
+#define _IF_    9
+#define _ELSE_  10
+#define _WHILE_ 11
+#define _DO_    12
 
-// Protótipo das funções ---------------------------------------------------
+// Símbolos -------------------------------------------------------------------
+#define _PARENTESE1_    13
+#define _PARENTESE2_    14
+#define _VIRGULA_       15
+#define _PVIRGULA_      16
+#define _CHAVE1_        17
+#define _CHAVE2_        18
+
+// Operadores -----------------------------------------------------------------
+#define _ATRIBUICAO_    19
+#define _SOMA_          20
+#define _SUBTRACAO_     21
+#define _MULTIPLICAO_   22
+#define _DIVISAO_       23
+
+// Operadores Relacionais ----------------------------------------------------- 
+#define _IGUALDADE_     24
+#define _DIFERENCA_     25
+#define _MAIOR_         26
+#define _MAIORIGUAL_    27
+#define _MENOR_         28
+#define _MENORIGUAL_    29
+
+// Outros ---------------------------------------------------------------------
+#define _ID_  30
+#define _NUM_ 31
+#define _COM_ 32
+
+// Protótipo das funções =======================================================
 int scanner(char palavra[]);
 
 
-// Implementação ------------------------------------------------------------
+// Implementação ===============================================================
 int scanner(char palavra[])
 {
-   char c;
-   int i = -1;
-   q0:
-    i++;
-    c = palavra[i];
-    if (c == 'a') goto q1;
-    else if (c == 'b') goto q0;
-    else if (c == '\0') return (_REJEITA_);
-    else goto poco;
-   q1:
-    i++;
-    c = palavra[i];
-    if (c == 'a') goto q1;
-    else if (c == 'b') goto q0;
-    else if (c == '\0') return (_ACEITA_);
-    else goto poco;
-   poco:
-    return (_REJEITA_);
+   return (-1);
 }
 
 
-// main -----------------------------------------------------------------------
+// main =======================================================================
 int main(int argc, char * argv[])
 {
-    char str[] = "bbababababaabba";
-    int out = scanner(str);
-    FILE * token_output_ptr = fopen("token.txt", "w");
-    fprintf(token_output_ptr, "%d", out);
-    fclose(token_output_ptr);
     return 0;
 }
 
