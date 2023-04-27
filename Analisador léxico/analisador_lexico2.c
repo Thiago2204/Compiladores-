b9a8a95a4040ff97e1305ac1b791f61abf214ba2
RysanDeluna <ry.marco.andrade@gmail.com>
2023-04-27 18:01:29 -0300

    Juncao de tudo - almost ready

--- Analisador léxico/analisador_lexico2.c
+++ Analisador léxico/analisador_lexico2.c
@@ -48,19 +48,22 @@
 #define _NUM_ 31 
 #define _COM_ 32
 
+
 // Protótipo das funções ======================================================
-int     scanner(char* palavra, char** atributo);
+int     scanner(char** palavra, char** atributo);
 void    token_output(char file_name[], int token[]);
 
 // Implementação ==============================================================
-int scanner(char* palavra, char** atributo)
+int scanner(char** palavra, char** atributo)
 {
     char c;
-    int attr_buffer = 1;
-    char* attr = (char*) malloc (sizeof(char) * attr_buffer);
+    int index = 0;
+    int qntde_elem = 1;
+    char* aux = (char*) malloc (sizeof(char));
+    
 
     q0: // somente neste estado haverá um switch case devido as diversas possibilidades
-        c = *palavra;
+        c = **palavra;
         switch (c)
         {
         case 'f': goto q71;
@@ -108,309 +111,339 @@ int scanner(char* palavra, char** atributo)
         
     // false
     q71:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'a') goto q72; else goto poco;
     q72:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'l') goto q73; else goto poco;
     q73:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 's') goto q74; else goto poco;
     q74:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'e') goto q75; else goto poco;
     q75:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q76; else goto poco;
     q76:
+        *(*palavra)++;
         return (_FALSE_);
     
     // true
     q66:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'r') goto q67; else goto poco;
     q67:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'u') goto q68; else goto poco;
     q68:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'e') goto q69; else goto poco;
     q69:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q70; else goto poco;
     q70:
+        *(*palavra)++;
         return (_TRUE_);
     
     // bool
     q61:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'o') goto q62; else goto poco;
     q62:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'o') goto q63; else goto poco;
     q63:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'l') goto q64; else goto poco;
     q64:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q65; else goto poco;
     q65:
+        *(*palavra)++;
         return (_BOOL_);
 
     // int - if
     q50:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'n') goto q51; else if (c == 'f') goto q54; else goto poco;
     q51:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 't') goto q52; else goto poco;
     q52:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q53; else goto poco;
     q53:
+        *(*palavra)++;
         return (_INT_);
     q54:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q55; else goto poco;
     q55:
+        *(*palavra)++;
         return (_IF_);
 
     // else
     q56:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'l') goto q57; else goto poco;
     q57:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 's') goto q58; else goto poco;
     q58:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'e') goto q59; else goto poco;
     q59:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q60; else goto poco;
     q60:
+        *(*palavra)++;
         return (_ELSE_);
 
     // print - proc
     q39:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'r') goto q40; else goto poco;
     q40:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'i') goto q41; else if (c == 'o') goto q45; else goto poco;
     q41:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'n') goto q42; else goto poco;
     q42:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 't') goto q43; else goto poco;
     q43:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q44; else goto poco;
     q44:
+        *(*palavra)++;
         return (_PRINT_);
     q45:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'c') goto q46; else goto poco;
     q46:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q47; else goto poco;
     q47: 
+        *(*palavra)++;
         return(_PROC_);
     
     // semic
     q77:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'e') goto q78; else goto poco;
     q78:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'm') goto q79; else goto poco;
     q79:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'i') goto q80; else goto poco;
     q80:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'c') goto q81; else goto poco;
     q81:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q82; else goto poco;
     q82:
+        *(*palavra)++;
         return (_SEMIC_);
 
     // void
     q83:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'o') goto q84; else goto poco;
     q84:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'i') goto q85; else goto poco;
     q85:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'd') goto q86; else goto poco;
     q86:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q87; else goto poco;
     q87:
+        *(*palavra)++;
         return (_VOID_);
 
     // while
     q88: 
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'h') goto q89; else goto poco;
     q89:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'i') goto q90; else goto poco;
     q90:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'l') goto q91; else goto poco;
     q91:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'e') goto q92; else goto poco;
     q92:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q93; else goto poco;
     q93:
+        *(*palavra)++;
         return (_WHILE_);
 
     // do
     q94:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == 'o') goto q95; else goto poco;
     q95:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q96; else goto poco;
     q96:
+        *(*palavra)++;
         return (_DO_);
 
     // Símbolos
     q31:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q33; else goto poco;
     q33:
+        *(*palavra)++;
         return (_PARENTESE1_);
     q27:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q34; else goto poco;
     q34:
+        *(*palavra)++;
         return (_PARENTESE2_);
     q28:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q35; else goto poco;
     q35:
+        *(*palavra)++;
         return (_VIRGULA_);
     q29:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q36; else goto poco;
     q36:
+        *(*palavra)++;
         return (_PVIRGULA_);
     q30:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q37; else goto poco;
     q37:
+        *(*palavra)++;
         return (_CHAVE1_);
     q32:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q38; else goto poco;
     q38:
+        *(*palavra)++;
         return (_CHAVE2_);
 
 	// Operadores
 	q1:
-		*(palavra++); c = *palavra; 
+		*(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q6; else if (c == '*') goto q2; else goto poco;
 	q6:
+        *(*palavra)++;
 		return (_DIVISAO_);
     q7:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q8; else goto poco;
     q8:
+        *(*palavra)++;
 		return (_SOMA_);
     q9:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q10; else goto poco;
     q10:
+        *(*palavra)++;
 		return (_SUBTRACAO_);
     q11:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q12; else goto poco;
     q12:
+        *(*palavra)++;
 		return (_MULTIPLICAO_);
     q13:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q14; else if (c == '>') goto q15; else if (c == '=') goto q17; else goto poco;
     q14:
+        *(*palavra)++;
 		return (_MENOR_);
     q15:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q16; else goto poco;
     q16:
+        *(*palavra)++;
 		return (_DIFERENCA_);
     q17:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q18; else goto poco;
     q18:
+        *(*palavra)++;
 		return (_MENORIGUAL_);
     q19:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q20; else if (c == '=') goto q21; else goto poco;
     q20:
+        *(*palavra)++;
 		return (_MAIOR_);
     q21:
-        *(palavra++); c = *palavra; 
+        *(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q22; else goto poco;
     q22:
+        *(*palavra)++;
 		return (_MAIORIGUAL_);
 	q23:
-		*(palavra++); c = *palavra; 
+		*(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q24; else if (c == '=') goto q25; else goto poco;
 	q24:
+        *(*palavra)++;
 		return (_ATRIBUICAO_);
 	q25:
-		*(palavra++); c = *palavra; 
+		*(*palavra)++; c = *palavra[0]; 
         if (c == ' ') goto q26; else goto poco;
 	q26:
+        *(*palavra)++;
 		return (_IGUALDADE_);
 
     // Comentario
 	q2:
-		*(palavra++); c = *palavra; 
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || (c == '_') || (c == '}') || (c == '{') || (c == ';') || (c == ',') || (c == '(') || (c == ')') || (c == '=') || (c == '>') || (c == '<') || (c == '-') || (c == '+') || (c == '/') || (c == ' ')) goto q2; else if (c == '*') goto q3; else goto poco;
+		*(*palavra)++; c = *palavra[0];
+        if (c == '*') goto q3; else goto q2;
 	q3:
-		*(palavra++); c = *palavra; 
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || (c == '_') || (c == '}') || (c == '{') || (c == ';') || (c == ',') || (c == '(') || (c == ')') || (c == '=') || (c == '>') || (c == '<') || (c == '-') || (c == '+') || (c == '*') || (c == ' ')) goto q3; else if (c == '/') goto q4; else goto poco;
+		*(*palavra)++; c = *palavra[0];
+        if (c == '/') goto q4; else if (c == '*') goto q3; else goto q2;
 	q4:
-		*(palavra++); c = *palavra; 
+		*(*palavra)++; c = *palavra[0];
         if (c == ' ') goto q5; else goto poco;
 	q5:
+        *(*palavra)++;
 		return (_COM_);
 
     // Numero
     q97:
-        *(palavra++); c = *palavra; 
+        aux[index] = *(*palavra); index++; qntde_elem++;
+        aux = (char*) realloc (aux, sizeof(char) * qntde_elem);
+    
+        *(*palavra)++; c = *palavra[0]; 
         if (c >= '0' && c <= '9')
-        {
-            attr[attr_buffer-1] = c;
-            attr = (char*) realloc (attr, sizeof(char) * (attr_buffer++));
             goto q97;
-        }  
         else if (c == ' ') goto q98; else goto poco;
 
     q98:
-        *atributo = attr;
+        *atributo = aux;
+        *(*palavra)++;
 		return (_NUM_);
 
     // Identificador
     q48:
-        *(palavra++); c = *palavra; 
+        aux[index] = *(*palavra); index++; qntde_elem++;
+        aux = (char*) realloc (aux, sizeof(char) * qntde_elem);
+    
+        *(*palavra)++; c = *palavra[0]; 
         if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        { 
-            attr[attr_buffer] = c;
-            attr = (char*) realloc (attr, sizeof(char) * (attr_buffer++));
             goto q48;
-        }
         else if (c == ' ') goto q49; else goto poco;
     q49:
-        *atributo = attr;
+        *atributo = aux;
+        *(*palavra)++;
         return (_ID_);
 
     // Poco
@@ -426,21 +459,20 @@ void token_output(char file_name[], int token[])
 }
 
 // main =======================================================================
-int main(int argc, char * argv[])
-{
-    char *str = "/* ** _x int */ ";
+int main(int argc, char* argv[]) {
+    char *line = "if _kma 123 /*ifas**/ > ";
+    FILE* output = fopen("sim.txt", "wa");
     int token;
-    char* attr;
+    char *attr;
     
-    // Apaga o conteúdo anterior do arquivo de saída
-    FILE* output = fopen(argv[1], "wa");  // O argumento é o nome do arquivo de saída
+    while (line[0] != '\0' && token != _ERROR_)
+    {
+        attr = "NONE";
+        token = scanner(&line, &attr);
+        printf("<%d,%s>\n", token, attr);
+        fprintf(output, "<%d,%s>\n", token, attr);
+    }
 
-    /* isso vai dentro do laço */
-    token = scanner(str, attr);
-    fprintf(output, "<%d, %s>", token, attr);
-    /* ----------------------- */
-
-
-    close(output);
+    fclose(output);
     return 0;
 }
