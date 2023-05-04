be6a4d9a8dad6583af6a64bb2c324bea4be1856a
RysanDeluna <ry.marco.andrade@gmail.com>
2023-05-04 15:28:03 -0300

    estrutura inicial do código

--- Analisador sintático/analisador_sintatico.c
+++ Analisador sintático/analisador_sintatico.c
@@ -20,33 +20,33 @@ int bloco (char palavra[], int *pos);
 
 //  Declarações
 int d_variavel_i(char palavra[], int *pos);
-int d_variavel(char palavra[], int *pos);
-int lista_id(char palavra[], int *pos);
-int decl_funcoes(char palavra[], int *pos);
-int declara_funcao(char palavra[], int *pos);
-int para_formal(char palavra[], int *pos);
+int d_variavel  (char palavra[], int *pos);
+int lista_id    (char palavra[], int *pos);
+int decl_func   (char palavra[], int *pos);
+int declara_func(char palavra[], int *pos);
+int para_formal (char palavra[], int *pos);
 
 //  Comandos
 int comando_comp(char palavra[], int *pos);
-int comando(char palavra[], int *pos);
-int atribuicao(char palavra[], int *pos);
+int comando     (char palavra[], int *pos);
+int atribuicao  (char palavra[], int *pos);
 int chamada_proc(char palavra[], int *pos);
-int parametro(char palavra[], int *pos);
+int parametro   (char palavra[], int *pos);
 int comando_cond(char palavra[], int *pos);
 int comando_repe(char palavra[], int *pos);
 
 //  Expressões
-int expressao(char palavra[], int *pos);
-int relacao(char palavra[], int *pos);
-int expre_sim(char palavra[], int *pos);
-int termo(char palavra[], int *pos);
-int fator(char palavra[], int *pos);
-int variavel(char palavra[], int *pos);
+int expressao   (char palavra[], int *pos);
+int relacao     (char palavra[], int *pos);
+int expre_sim   (char palavra[], int *pos);
+int termo       (char palavra[], int *pos);
+int fator       (char palavra[], int *pos);
+int variavel    (char palavra[], int *pos);
 
 //  Numeros e Identificadores
-int bool(char palavra[], int *pos);
-int num(char palavra[], int *pos);
-int ident(char palavra[], int *pos);
+int bool    (char palavra[], int *pos);
+int num     (char palavra[], int *pos);
+int ident   (char palavra[], int *pos);
 
 //  Implementação das Funções =======================================
 
